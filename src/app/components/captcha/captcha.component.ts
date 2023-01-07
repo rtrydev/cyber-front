04464171aa009593e296dbeb2edfe66726b95a54
rtrydev <r.kulka44@gmail.com>
2023-01-07 19:13:35 +0100

    Ui improvements

--- src/app/components/captcha/captcha.component.ts
+++ src/app/components/captcha/captcha.component.ts
@@ -12,11 +12,40 @@ export class CaptchaComponent implements OnInit {
   captchaImg: string | null = null;
   captchaOptions: { index: number; image: string; }[] | null = null;
 
+  isDuringCheck: boolean = false;
+  checkedIndex: number = -1;
+  captchaValidState: boolean = false;
+  captchaFailed: boolean = false;
+
   constructor(private captchaService: CaptchaService) { }
 
   ngOnInit(): void {
+    this.loadCaptcha();
+  }
+
+  public selectPiece(index: number) {
+    if(this.isDuringCheck || this.captchaValidState) {
+      return;
+    }
+
+    this.checkedIndex = index;
+    this.captchaFailed = false;
+    this.isDuringCheck = true;
+
+    this.captchaService.sendAnswer(this.captchaData?.id || "", index)
+      .subscribe(result => {
+        if (result.success) {
+          this.captchaValidState = true;
+          this.checkedIndex = -1;
+        } else {
+          this.loadCaptcha();
+        }
+      });
+  }
+
+  private loadCaptcha() {
     this.captchaService.getCaptcha().subscribe(result => {
-      this.captchaData = JSON.parse(result);
+      this.captchaData = result as ICaptchaData;
 
       this.captchaImg = "data:image/jpg;base64," + this.captchaData?.big_img;
       const small_images = this.captchaData?.small_imgs.map(
@@ -29,6 +58,12 @@ export class CaptchaComponent implements OnInit {
       );
 
       this.captchaOptions = small_images?.sort(_ => (Math.random() > .5) ? 1 : -1) || null;
+      this.isDuringCheck = false;
+
+      if (this.checkedIndex !== -1) {
+        this.checkedIndex = -1;
+        this.captchaFailed = true;
+      }
     });
   }
 
--- src/app/services/captcha.service.ts
+++ src/app/services/captcha.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
+import {ICaptchaResult} from "../interfaces/ICaptchaResult";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,10 @@ export class CaptchaService {
   constructor(private httpClient: HttpClient) { }
 
   public getCaptcha() {
-    return this.httpClient.post(`${this.captchaApiUrl}/start`, {}, {responseType: 'text'});
+    return this.httpClient.post(`${this.captchaApiUrl}/start`, {});
+  }
+
+  public sendAnswer(captchaId: string, answer: number) {
+    return this.httpClient.post<ICaptchaResult>(`${this.captchaApiUrl}/answer?id=${captchaId}&answer=${answer}`, {})
   }
 }
